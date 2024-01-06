24ef0e1c8b2ddc05f3763c1a1cf97dc37aaafad8
hitrocs-polito <su12057@studenti.polito.it>
2024-01-06 15:05:52 +0500

    Update CartWithQuantity.jsx

--- src/components/CartWithQuantity.jsx
+++ src/components/CartWithQuantity.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import styled from 'styled-components';
-import { FaShoppingCart } from 'react-icons/fa';
+// import { FaShoppingCart } from 'react-icons/fa';
 
 const CartContainer = styled.div`
   display: flex;
@@ -18,21 +18,21 @@ const QuantityControl = styled.div`
 `;
 
 const QuantityButton = styled.button`
-  background-color: #eee;
+  background-color: #EAEAEA;
   border: none;
   margin: 0 2px;
   cursor: pointer;
 `;
 
 const CartCounter = styled.span`
-  background-color: #FFA542;
-  color: white;
-  border-radius: 50%;
-  padding: 1px 6px;
+  /* background-color: #FFA542; */
+  /* color: white; */
+  /* border-radius: 50%;
+  padding: 4px 9px;
   font-size: 20px;
   min-width: 15px;
   text-align: center; 
-  font-weight: 500;
+  font-weight: 500; */
   user-select: none;
 `
 
@@ -65,15 +65,26 @@ function CartWithQuantity({onClick}) {
   return (
     <CartContainer>
       {showCart ? (
-        <FaShoppingCart size={30} color="#FFA542" onClick={toggleCart} />
+        <svg xmlns="http://www.w3.org/2000/svg"  onClick={toggleCart} fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="orange" width={'2rem'} class="w-6 h-6">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
+        </svg>    
+        // <FaShoppingCart size={30} color="#FFA542" onClick={toggleCart} />
       ) : (
         <QuantityControl>
           <QuantityButton onClick={handleDecrement}>
-            <CartCounter>–</CartCounter>
+            <CartCounter>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" width={'2rem'} stroke="orange" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M15 12H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+</svg>
+</CartCounter>
           </QuantityButton>
           <div>{quantity}</div>
           <QuantityButton onClick={handleIncrement}>
-            <CartCounter>+</CartCounter>
+            <CartCounter>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" width={'2rem'} stroke="orange" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+              </svg>
+            </CartCounter>
           </QuantityButton>
         </QuantityControl>
       )}
